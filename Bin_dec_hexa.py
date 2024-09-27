84445af2557a91c46e4dc4138c4c47fac43ad3ea
Zeussurvival <emiljokui@gmail.com>
2024-09-27 13:10:40 +0200

    tri

---
+++ Bin_dec_hexa.py
@@ -0,0 +1,23 @@
+# Pour les fonctions
+
+# bin_other est une fonction qui passe du binaire aux autres bases
+def bin_other(number,base_ori,base_fin):
+    number_fin = ""
+    if base_ori == base_fin:
+        return number
+
+    if base_ori == 2:
+        if base_fin == 10:
+
+        if base_fin == 16:
+
+    # if base_ori == 10:
+    #     if base_fin == 2:
+        
+    #     if base_fin == 16:
+    
+    # if base_ori == 16:
+    #     if base_fin == 2:
+
+    #     if base_fin == 10:
+print(bin_other("5",10,2))
