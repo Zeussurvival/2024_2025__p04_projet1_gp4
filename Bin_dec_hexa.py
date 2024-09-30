4b8f6c94a0c637be500c923d861aa791c796deee
Zeussurvival <emiljokui@gmail.com>
2024-09-30 09:35:21 +0200

    Premier bon
    Base 2 --> Base 10 OK

--- Bin_dec_hexa.py
+++ Bin_dec_hexa.py
@@ -1,23 +1,40 @@
 # Pour les fonctions
 
+
 # bin_other est une fonction qui passe du binaire aux autres bases
 def bin_other(number,base_ori,base_fin):
-    number_fin = 0
+    # Variables necessaires (todo: a expliquer l'utilité)
+
+    decompostion_l = []
+    number_finale = 0
     number_test = number
+    number_quon_regarde = number
     number_dizaine = 0
     if base_ori == base_fin:
         return number
-    
+
+
+    # Connaitres le nombre des dizaines pour faire 1 ^0, 0 ^1, 1 ^2, etc ...
     while number_test > 1:
         number_test = round(number_test / 10)
         number_dizaine += 1
-        
+
+    number_test = number
+    # Decomposition d'un nombre en liste
+    for i in range(number_dizaine + 1):
+        number_quon_regarde = number_test % 10
+        number_test //= 10
+        decompostion_l.append(number_quon_regarde)
+    print(decompostion_l)
+
+
     if base_ori == 2:
         if base_fin == 10:
-
-            print(number_dizaine)
+            for i in range(len(decompostion_l)):
+                number_finale += decompostion_l[i] * (2 **i)
+                print(decompostion_l[i] * (2 ** i),i)
             
-        #if base_fin == 16:
+        # if base_fin == 16:
 
     # if base_ori == 10:
     #     if base_fin == 2:
@@ -28,4 +45,7 @@ def bin_other(number,base_ori,base_fin):
     #     if base_fin == 2:
 
     #     if base_fin == 10:
-print(bin_other(101,2,10))
+
+    return number_finale
+
+print(bin_other(11001,2,10))
