2a2c46f2f89bba8bca55c7126769f2eba2955af2
Zeussurvival <emiljokui@gmail.com>
2024-10-18 13:10:14 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -15,10 +15,12 @@ running = True
 
 color_fond = (243,60,21)
 WHITE = (255,255,255)
+BLUE_STYLEE = (14, 190, 235)
 white_rect_haut = (25,25,screen_width - 50,100)
 
 base_start_box = (25,150,400,50)
 base_end_box = (screen_width - 425,150,400,50)
+finish_box = (screen_width/2 - 200,150,screen_width/2 - 240,50)
 
 while running:
     for event in pygame.event.get():
@@ -30,6 +32,7 @@ while running:
     pygame.draw.rect(screen,WHITE,white_rect_haut)
     pygame.draw.rect(screen,WHITE,base_start_box)
     pygame.draw.rect(screen,WHITE,base_end_box)
+    pygame.draw.rect(screen,BLUE_STYLEE,finish_box)
 
     pygame.display.flip()
 
